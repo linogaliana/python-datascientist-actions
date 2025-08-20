f40a8bfcd59dd60822d1457599d0b1f29f069e3f
lgaliana <lino.galiana@insee.fr>
2025-08-20 08:16:12 +0000

    Actions

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -4,15 +4,15 @@ set -e
 DEST_REPO_NAME="$1"
 ECHO="$2"
 
-
 echo "::group::🧹 Preparing directory"
 rm _quarto.yml
 cp _quarto-prod.yml _quarto.yml
+
+# Remove files not building in ipynb
 rm index.qmd
 rm -rf content/git/
 rm content/annexes/evaluation.qmd
 echo "::endgroup::"
-
 
 echo "::group::🚀 Converting to ipynb"
 export QUARTO_PROFILE=fr,en
@@ -26,25 +26,19 @@ uv run quarto render --profile fr --to ipynb --execute $ECHO_FLAG
 uv run quarto render --profile en --to ipynb --execute $ECHO_FLAG
 echo "::endgroup::"
 
-
 echo "::group::📦 Moving files"
 mkdir -p temp_notebooks/notebooks
 uv run build/move_files.py --direction temp_notebooks/notebooks
 echo "::endgroup::"
 
-
-echo "::group::🔁 Pushing to ${DEST_REPO_NAME}"
-gh workflow run linogaliana/github-action-push-to-another-repository@main \
-  --repo linogaliana/"$DEST_REPO_NAME" \
-  --env API_TOKEN_GITHUB="${API_TOKEN_GITHUB}" \
-  --ref main \
-  --json '{
-    "source-directory": "temp_notebooks/",
-    "destination-repository-username": "linogaliana",
-    "destination-repository-name": "'"${DEST_REPO_NAME}"'",
-    "user-email": "lino.galiana@insee.fr",
-    "destination-github-username": "linogaliana",
-    "create-target-branch-if-needed": true,
-    "reset-repo": true
-  }'
+echo "::group::🚀 Pushing notebooks to $DEST_REPO_NAME"
+uv run github-action-push-to-another-repository \
+  --source-directory temp_notebooks/ \
+  --destination-repository-username linogaliana \
+  --destination-repository-name "$DEST_REPO_NAME" \
+  --destination-github-username linogaliana \
+  --user-email lino.galiana@insee.fr \
+  --create-target-branch-if-needed \
+  --reset-repo \
+  --token "$API_TOKEN_GITHUB"
 echo "::endgroup::"
